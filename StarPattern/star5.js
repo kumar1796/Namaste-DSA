44cc4bf2154929339e38a0fdfced675925010ada
Dinesh Kummar <dk23818@gmail.com>
2025-07-05 17:43:06 +0530

    Chapter : Star Patterns

---
+++ StarPattern/star5.js
@@ -0,0 +1,26 @@
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+let n = 5;
+
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j < n - i; j++) {
+    row = row + (j + 1) + " ";
+    // row = row + "* "  // for start pattern
+  }
+  console.log(row);
+}
+
+// for (let i = 5; i > 0; i--) {
+//   let row = "";
+//   for (let j = 0; j < i; j++) {
+//     row = row + (j + 1) + " ";
+//   }
+//   console.log(row);
+// }
+
+
